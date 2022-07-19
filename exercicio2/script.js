17964161788ced905f74f3badb344269cd2a1317
myllenavieira <vieirasilvamyllena@gmail.com>
2022-07-18 22:26:49 -0300

    Exercício Prática a007

---
+++ exercicio2/script.js
@@ -0,0 +1,12 @@
+const pergunta1 = "Você gosta de usar roupas escuras?";
+const resposta1 = prompt(pergunta1);
+const pergunta2 = "Você tem pavor ao sol?";
+const resposta2 = prompt(pergunta2);
+const pergunta3 = "Você odeia pizza de alho?";
+const resposta3 = prompt(pergunta3);
+
+console.log(pergunta1, " - ", resposta1);
+console.log(pergunta2, " - ", resposta2);
+console.log(pergunta3, " - ", resposta3);
+
+confirm("Você é um vampiro?");
